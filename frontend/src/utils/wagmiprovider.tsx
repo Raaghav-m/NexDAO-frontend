551fa3a546e79f92698fd2179f9f5d0fbf433e0a
Soham Kayal <sohamkayal@Sohams-Laptop.local>
2024-10-19 05:40:08 -0700

    Added rainbowkit
    Signed-off-by: Soham Kayal <sohamkayal@Sohams-Laptop.local>

---
+++ frontend/src/utils/wagmiprovider.tsx
@@ -0,0 +1,61 @@
+// @ts-nocheck comment
+import {
+  connectorsForWallets,
+  RainbowKitProvider,
+  darkTheme,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import {
+  argentWallet,
+  coinbaseWallet,
+  imTokenWallet,
+  injectedWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  omniWallet,
+  rainbowWallet,
+  trustWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { configureChains, createConfig, WagmiProvider, http } from "wagmi";
+
+import { polygonZkEvmCardona } from "wagmi/chains";
+
+const config = getDefaultConfig({
+  appName: "ETHSF",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  chains: [polygonZkEvmCardona],
+  transports: {
+    [polygonZkEvmCardona.id]: http(),
+  },
+});
+const queryClient = new QueryClient();
+
+function WagmiConnect(props: any) {
+  return (
+    <>
+      {config && (
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider
+              theme={darkTheme({
+                accentColor: "#1E88E5",
+                borderRadius: "large",
+                overlayBlur: "small",
+              })}
+              coolMode
+            >
+              {props.children}
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
+      )}
+    </>
+  );
+}
+
+export default WagmiConnect;
