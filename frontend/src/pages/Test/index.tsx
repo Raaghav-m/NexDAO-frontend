19b15b1d12f763d835f7c455941bec3d9fcd874a
Soham Kayal <sohamkayal@Sohams-Laptop.local>
2024-10-19 15:11:35 -0700

    Added Test Page
    Signed-off-by: Soham Kayal <sohamkayal@Sohams-Laptop.local>

---
+++ frontend/src/pages/Test/index.tsx
@@ -0,0 +1,100 @@
+//@ts-nocheck comment
+import React from "react";
+import { ethers } from "ethers";
+import usersideabi from "../../utils/abis/usersideabi.json";
+import { useAccount } from "wagmi";
+import { useState, useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
+
+const index = () => {
+  const account = useAccount();
+  const [chainId, setChainId] = useState(0);
+  const toast = useToast();
+
+  useEffect(() => {
+    const updateChainId = async () => {
+      if (account) {
+        setChainId(account.chainId);
+      }
+    };
+
+    updateChainId();
+  }, [account]);
+
+  const handleSubmit = async (e) => {
+    if (account.isConnected) {
+      console.log("account connected");
+      console.log(`chainId is: ${chainId}`);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      let contract;
+      // flow === 545
+      //polygonZkEvmCardona === 2442
+      // skaleCalypsoTestnet === 974399131
+      //sepolia === 11155111
+      if (chainId === 545) {
+        contract = new ethers.Contract(
+          process.env.NEXT_PUBLIC_USERSIDE_FLOW_ADDRESS,
+          usersideabi,
+          signer
+        );
+      } else if (chainId === 2442) {
+        contract = new ethers.Contract(
+          process.env.NEXT_PUBLIC_USERSIDE_CARDONA_ADDRESS,
+          usersideabi,
+          signer
+        );
+      } else if (chainId === 974399131) {
+        contract = new ethers.Contract(
+          process.env.NEXT_PUBLIC_USERSIDE_SKALE_ADDRESS,
+          usersideabi,
+          signer
+        );
+      } else if (chainId === 11155111) {
+        contract = new ethers.Contract(
+          process.env.NEXT_PUBLIC_USERSIDE_SEPOLIA_ADDRESS,
+          usersideabi,
+          signer
+        );
+      }
+      const accounts = await provider.listAccounts();
+      const userRes = await contract.userIdtoUser(BigInt(1));
+      console.log(userRes);
+    } else {
+      console.log("no account connected");
+      toast({
+        title: "No Account Connected",
+        description: "Please connect your wallet.",
+        status: "error",
+        duration: 1000,
+        isClosable: true,
+        position: "top-right",
+      });
+    }
+    // if (window.ethereum._state.accounts.length !== 0) {
+    //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+    //   const signer = provider.getSigner();
+    //   const contract = new ethers.Contract(
+    //     process.env.NEXT_PUBLIC_USERSIDE_SKALE_ADDRESS,
+    //     usersideabi,
+    //     signer
+    //   );
+    //   const accounts = await provider.listAccounts();
+    //   const userRes = await contract.userIdtoUser(BigInt(1));
+    //   console.log(userRes);
+    // }
+  };
+  return (
+    <div>
+      <button
+        onClick={() => {
+          handleSubmit();
+        }}
+      >
+        Test Btn
+      </button>
+    </div>
+  );
+};
+
+export default index;
